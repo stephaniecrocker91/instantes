b9555d1bb0178b5d41ec6aa423f6cfc968a90836
Stephie <stephaniecrocker91@gmail.com>
2022-09-27 13:47:28 +0000

    Add js script tags to index.html

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
                 <Navbar.Brand href="#home">Instantes</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
+                    <Nav className="ml-auto">
                         <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
                         <Nav.Link><i className="fas fa-arrow-right"></i>Sign In</Nav.Link>
                         <Nav.Link><i className="fas fa-feather"></i>Sign Up</Nav.Link>
