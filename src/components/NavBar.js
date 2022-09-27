4ac4673bedb4cf7d141b9b321180b04f54640edc
Stephie <stephaniecrocker91@gmail.com>
2022-09-27 15:12:51 +0000

    NavBar styls written as test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Navbar, Container, Nav } from "react-bootstrap";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
     return (
-        <Navbar bg="light" expand="md" fixed="top">
+        <Navbar className={styles.NavBar} bg="light" expand="md" fixed="top">
             <Container>
                 <Navbar.Brand href="#home">Instantes</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
