d8ac91b03362838ff932fb9ee8fc5c616ce44c9b
Stephie <stephaniecrocker91@gmail.com>
2022-09-28 13:16:04 +0000

    Styled navar to have dark bootstrap react feature

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,11 +4,11 @@ import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
     return (
-        <Navbar className={styles.NavBar} expand="md" fixed="top">
+        <Navbar className={styles.NavBar} expand="md" fixed="top" variant="dark">
             <Container>
                 <Navbar.Brand className={styles.Brand} href="#home">Instant<span className={styles.Icon}>es</span></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav" className={styles.NavLink}>
+                <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
                         <Nav.Link className={styles.NavLink}><i className="fas fa-home"></i>Home</Nav.Link>
                         <Nav.Link className={styles.NavLink}><i className="fas fa-arrow-right"></i>Sign In</Nav.Link>
@@ -20,5 +20,4 @@ const NavBar = () => {
         </Navbar>
     );
 };
-
 export default NavBar
