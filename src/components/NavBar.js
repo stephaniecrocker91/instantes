f7a6edd54a6f12a1ae43ce77728a45ae788466a8
Stephie <stephaniecrocker91@gmail.com>
2022-09-28 11:34:30 +0000

    Install react-router, import Broweser router to index.js, import Route and Switch to app.js, add swicth routes to container: homepage, signin, signup, and page not found

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,15 +4,15 @@ import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
     return (
-        <Navbar className={styles.NavBar} bg="dark" expand="md" fixed="top">
+        <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
                 <Navbar.Brand className={styles.Brand} href="#home">Instant<span className={styles.Icon}>es</span></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Collapse id="basic-navbar-nav" className={styles.NavLink}>
                     <Nav className="ml-auto">
-                        <Nav.Link className={styles.WhiteFont}><i className="fas fa-home"></i>Home</Nav.Link>
-                        <Nav.Link className={styles.WhiteFont}><i className="fas fa-arrow-right"></i>Sign In</Nav.Link>
-                        <Nav.Link className={styles.WhiteFont}><i className="fas fa-feather"></i>Sign Up</Nav.Link>
+                        <Nav.Link className={styles.NavLink}><i className="fas fa-home"></i>Home</Nav.Link>
+                        <Nav.Link className={styles.NavLink}><i className="fas fa-arrow-right"></i>Sign In</Nav.Link>
+                        <Nav.Link className={styles.NavLink}><i className="fas fa-feather"></i>Sign Up</Nav.Link>
                     </Nav>
 
                 </Navbar.Collapse>
