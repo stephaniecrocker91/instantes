4d8e3ea1490235e788c57f7ed3184b3475e9fd56
Stephie <stephaniecrocker91@gmail.com>
2022-09-28 10:29:59 +0000

    Styled navbar fontawesome icons, and logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,15 +4,15 @@ import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
     return (
-        <Navbar className={styles.NavBar} bg="light" expand="md" fixed="top">
+        <Navbar className={styles.NavBar} bg="dark" expand="md" fixed="top">
             <Container>
-                <Navbar.Brand className={styles.Brand} href="#home">Instantes</Navbar.Brand>
+                <Navbar.Brand className={styles.Brand} href="#home">Instant<span className={styles.Icon}>es</span></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
-                        <Nav.Link><i className="fas fa-arrow-right"></i>Sign In</Nav.Link>
-                        <Nav.Link><i className="fas fa-feather"></i>Sign Up</Nav.Link>
+                        <Nav.Link className={styles.WhiteFont}><i className="fas fa-home"></i>Home</Nav.Link>
+                        <Nav.Link className={styles.WhiteFont}><i className="fas fa-arrow-right"></i>Sign In</Nav.Link>
+                        <Nav.Link className={styles.WhiteFont}><i className="fas fa-feather"></i>Sign Up</Nav.Link>
                     </Nav>
 
                 </Navbar.Collapse>
