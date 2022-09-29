cfd757bdd031d44f2201a659a4c04010bcea5035
Stephie <stephaniecrocker91@gmail.com>
2022-09-29 15:49:26 +0000

    Create handleSubmit function catching errors, and displaying them in boostrap alerts under each input field in SignUpForm.js

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
+
+import axios from "axios";
 
 const SignUpForm = () => {
 
@@ -16,6 +18,10 @@ const SignUpForm = () => {
   });
   const { username, password1, password2 } = signUpData;
   
+  const [errors, setErrors] = useState({});
+  
+  const history = useHistory();
+
   const handleChange = (event) => {
     setSignUpData({
       ...signUpData,
@@ -23,12 +29,23 @@ const SignUpForm = () => {
     });
   };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/registration/", signUpData);
+      history.push("/signin");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
   return (
     <Row className={styles.Row}>
       <Col className="my-auto py-2 p-md-2" md={6}>
         <Container className={`${appStyles.Content} ${appStyles.Pink} p-4 `}>
           <h1 className={styles.Header}>Sign up</h1>
-          <Form>
+          
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
               <Form.Control 
@@ -39,7 +56,13 @@ const SignUpForm = () => {
                 value={username} 
                 onChange={handleChange} />
             </Form.Group>
-          
+            {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+
             <Form.Group controlId="password1">
               <Form.Label className="d-none">Password</Form.Label>
               <Form.Control 
@@ -51,6 +74,12 @@ const SignUpForm = () => {
                 onChange={handleChange} />
             </Form.Group>
 
+            {errors.password1?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
+
             <Form.Group controlId="password2">
               <Form.Label className="d-none">Password</Form.Label>
               <Form.Control 
@@ -61,6 +90,12 @@ const SignUpForm = () => {
                 value={password2}
                 onChange={handleChange} />
             </Form.Group>
+
+            {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
 
             <Button className={`${btnStyles.Button} ${btnStyles.Wide}`} type="submit">
             Sign Up
