712b720a28a09fc5f29a2f4117dd328d2a65758a
Stephie <stephaniecrocker91@gmail.com>
2022-09-30 14:54:00 +0000

    Create context object, wrap children components in Context Objects, pass data in value prop and access data by calling useContext(contextObject) to display correct navbar icons depending on which user is logged in or not

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useContext } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../App";
+
 
 const NavBar = () => {
+    
+    const currentUser = useContext(CurrentUserContext);
+    const loggedInIcons = <><div className={styles.NavLink}  >{currentUser?.username}</div></>
+    const loggedOutIcons = (
+        <>
+            <NavLink 
+            to="/signin" 
+            className={styles.NavLink} 
+            activeClassName={styles.Active}
+            >
+                <i className="fas fa-arrow-right"></i>Sign In
+            </NavLink>
+            <NavLink 
+            to="/signup" 
+            className={styles.NavLink} 
+            activeClassName={styles.Active}
+            >
+                <i className="fas fa-feather"></i>Sign Up
+            </NavLink>
+        </>
+    )
+
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top" variant="dark">
             <Container>
@@ -21,20 +45,7 @@ const NavBar = () => {
                             >
                             <i className="fas fa-home"></i>Home
                         </NavLink>
-                        <NavLink 
-                            to="/signin" 
-                            className={styles.NavLink} 
-                            activeClassName={styles.Active}
-                            >
-                            <i className="fas fa-arrow-right"></i>Sign In
-                        </NavLink>
-                        <NavLink 
-                            to="/signup" 
-                            className={styles.NavLink} 
-                            activeClassName={styles.Active}
-                            >
-                            <i className="fas fa-feather"></i>Sign Up
-                        </NavLink>
+                        {currentUser ? loggedInIcons : loggedOutIcons}
                     </Nav>
 
                 </Navbar.Collapse>
