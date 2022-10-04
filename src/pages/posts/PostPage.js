9f86a2f03fb5f322f0c6eb35170069b82d9a6934
Stephie <stephaniecrocker91@gmail.com>
2022-10-04 13:35:38 +0000

    Write useEffect handleMount function for PostPage to retreve data from API

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import { useParams } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function PostPage() {
-  // Add your logic here
+    const { id } = useParams();
+    const [post, setPost] = useState({ results: [] });
 
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const [{ data: post }] = await Promise.all([
+                axiosReq.get(`/posts/${id}`),
+                ]);
+                setPost({ results: [post] });
+                console.log(post);
+            } catch (err) {
+                console.log(err);
+            }
+            };
+        
+            handleMount();
+        }, [id]);
 
     return (
         <Row className="h-100">
