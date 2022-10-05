8bbe4c8024c17d79fffa2c9bec028f7f8243b294
Stephie <stephaniecrocker91@gmail.com>
2022-10-05 12:41:36 +0000

    Wrrite handleUnike function and call it  when clicking heart icon and li

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -45,6 +45,22 @@ const Post = (props) => {
             }
         };
 
+        const handleUnlike = async () => {
+            try {
+                await axiosRes.delete(`/likes/${like_id}/`);
+                setPosts((prevPosts) => ({
+                    ...prevPosts,
+                    results: prevPosts.results.map((post) => {
+                    return post.id === id
+                        ? { ...post, likes_count: post.likes_count - 1, like_id: null }
+                        : post;
+                    }),
+                }));
+                } catch (err) {
+                console.log(err);
+                }
+            };
+
     return (
         <Card className={styles.Post}>
             <Card.Body>
@@ -74,7 +90,7 @@ const Post = (props) => {
                         <i className="far fa-heart" />
                         </OverlayTrigger>
                     ) : like_id ? (
-                        <span onClick={() => {}}>
+                        <span onClick={handleUnlike}>
                         <i className={`fas fa-heart ${styles.Heart}`} />
                         </span>
                     ) : currentUser ? (
