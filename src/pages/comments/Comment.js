12f8812189b10d0168b39c5749fe142b67d96bd8
Stephie <stephaniecrocker91@gmail.com>
2022-10-10 15:23:48 +0000

    Import MoreDropdown component and useCurrentUser

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -3,6 +3,8 @@ import { Media } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import styles from "../../styles/Comment.module.css";
+import { useCurrentUser } from "../../context/CurrentUserContext";
+import { MoreDropdown } from "../../components/MoreDropdown";
 
 const Comment = (props) => {
     const { profile_id, profile_image, owner, updated_at, content } = props;
@@ -23,7 +25,7 @@ const Comment = (props) => {
                     <p>{content}</p>
                 </Media.Body>
                 {is_owner && (
-                    <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete} />
+                    <MoreDropdown handleEdit={() => {}} handleDelete={() => {}} />
                 )}
             </Media>
         </div>
