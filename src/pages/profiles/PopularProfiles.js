5f7d58cde8ab14e41499631a71af049fc793932b
Stephie <stephaniecrocker91@gmail.com>
2022-10-10 18:24:55 +0000

    Wrire useEffect handleMount code, to fetch data and order according to follower count in descending order. Map results in container

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { Container } from 'react-bootstrap'
+import { axiosReq } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../context/CurrentUserContext";
 import appStyles from '../../App.module.css'
 
 const PopularProfiles = () => {
@@ -10,10 +12,31 @@ const PopularProfiles = () => {
     });
     const { popularProfiles } = profileData;
     const currentUser = useCurrentUser();
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const { data } = await axiosReq.get(
+                "/profiles/?ordering=-followers_count"
+                );
+                setProfileData((prevState) => ({
+                ...prevState,
+                popularProfiles: data,
+                }));
+            } catch (err) {
+                console.log(err);
+            }
+            };
+        
+            handleMount();
+        }, [currentUser]);
     
     return (
         <Container className={`${appStyles.Content} ${appStyles.Boxed}`}>
-            <p>Most followed profiles.</p>
+            <p>Top rated profiles.</p>
+            {popularProfiles.results.map((profile) => (
+            <p key={profile.id}>{profile.owner}</p>
+            ))}
         </Container>
     )
 }
