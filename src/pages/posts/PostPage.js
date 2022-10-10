21fec58f95dbec1dddebf86b196d54df733b1e83
Stephie <stephaniecrocker91@gmail.com>
2022-10-10 14:03:25 +0000

    Create CreateCommentForm.js and import container component to postpage.js, add relevant imports

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -9,9 +9,16 @@ import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../context/CurrentUserContext";
+
 function PostPage() {
     const { id } = useParams();
     const [post, setPost] = useState({ results: [] });
+
+    const currentUser = useCurrentUser();
+    const profile_image = currentUser?.profile_image;
+    const [comments, setComments] = useState({ results: [] });
 
     useEffect(() => {
         const handleMount = async () => {
@@ -24,8 +31,7 @@ function PostPage() {
             } catch (err) {
                 console.log(err);
             }
-            };
-        
+            };      
             handleMount();
         }, [id]);
 
@@ -35,7 +41,17 @@ function PostPage() {
             <p>Popular profiles for mobile</p>
             <Post {...post.results[0]} setPosts={setPost} postPage />
             <Container className={appStyles.Content}>
-            Comments
+            {currentUser ? (
+                <CommentCreateForm
+                    profile_id={currentUser.profile_id}
+                    profileImage={profile_image}
+                    post={id}
+                    setPost={setPost}
+                    setComments={setComments}
+                />
+                ) : comments.results.length ? (
+                "Comments"
+                ) : null}              
             </Container>
         </Col>
         <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
