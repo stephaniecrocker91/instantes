265dea9532c285f80af363145cc2e59b4841751b
Stephie <stephaniecrocker91@gmail.com>
2022-10-11 09:48:38 +0000

    In Profile.js pass in props, and destructureprofile and mobile props, give image default value of 55 size, detsructure profile props, and check if current user logged in is owner of profile

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -2,7 +2,14 @@ import React from 'react'
 import styles from "../../styles/Profile.module.css";
 import btnStyles from "../../styles/Button.module.css";
 
-const Profile = () => {
+const Profile = (props) => {
+
+    const { profile, mobile, imageSize = 55 } = props;
+    const { id, following_id, image, owner } = profile;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+
     return (
         <div>Profile</div>
     )
