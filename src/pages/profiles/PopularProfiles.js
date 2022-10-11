23fdef8932f5c1653a92857945508fe56ecf1a78
Stephie <stephaniecrocker91@gmail.com>
2022-10-11 09:06:18 +0000

    Customise dout poplarprofile fields to be responsive for mobile devices

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -5,7 +5,7 @@ import { useCurrentUser } from "../../context/CurrentUserContext";
 import Asset from "../../components/Asset";
 import appStyles from '../../App.module.css'
 
-const PopularProfiles = () => {
+const PopularProfiles = ({ mobile }) => {
     const [profileData, setProfileData] = useState({
         // we will use the pageProfile later!
         pageProfile: { results: [] },
@@ -33,19 +33,31 @@ const PopularProfiles = () => {
         }, [currentUser]);
     
     return (
-        <Container className={appStyles.Content}>
-        {popularProfiles.results.length ? (
-            <>
-                <p>Most followed profiles.</p>
-                {popularProfiles.results.map((profile) => (
-                <p key={profile.id}>{profile.owner}</p>
-                ))}
-            </>
-            ) : (
-            <Asset spinner />
-            )}
+        <Container
+        className={`${appStyles.Content} ${appStyles.Boxed} ${
+            mobile && "d-lg-none text-center mb-3"
+          }`}
+        >
+            {popularProfiles.results.length ? (
+                <>
+                    <p>Most followed profiles.</p>
+                    {mobile ? (
+                        <div className="d-flex justify-content-around">
+                        {popularProfiles.results.slice(0, 4).map((profile) => (
+                            <p key={profile.id}>{profile.owner}</p>
+                        ))}
+                        </div>
+                    ) : (
+                        popularProfiles.results.map((profile) => (
+                        <p key={profile.id}>{profile.owner}</p>
+                        ))
+                    )}
+                    </>
+                ) : (
+                    <Asset spinner />
+                )}
         </Container>
-        );
-    };
+    );
+};
 
 export default PopularProfiles
