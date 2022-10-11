cdd503d8c5899387970c1d38d387036109f6ae12
Stephie <stephaniecrocker91@gmail.com>
2022-10-11 10:09:36 +0000

    Create profile component to display user image, usernames and follow/unfollow depending on wether users are logge din, and they are followed already or not

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -4,6 +4,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 import { useCurrentUser } from "../../context/CurrentUserContext";
 import Asset from "../../components/Asset";
 import appStyles from '../../App.module.css'
+import Profile from "./Profile";
 
 const PopularProfiles = ({ mobile }) => {
     const [profileData, setProfileData] = useState({
--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "../../styles/Profile.module.css";
 import btnStyles from "../../styles/Button.module.css";
+import { Link } from 'react-router-dom';
+import { useCurrentUser } from "../../context/CurrentUserContext";
+import Avatar from '../../components/Avatar';
+import { Button } from "react-bootstrap";
 
 const Profile = (props) => {
 
@@ -11,7 +15,38 @@ const Profile = (props) => {
     const is_owner = currentUser?.username === owner;
 
     return (
-        <div>Profile</div>
+        <div
+            className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
+        >
+            <div>
+            <Link className="align-self-center" to={`/profiles/${id}`}>
+                <Avatar src={image} height={imageSize} />
+            </Link>
+            </div>
+            <div className={`mx-2 ${styles.WordBreak}`}>
+                <strong>{owner}</strong>
+            </div>
+            <div className={`text-right ${!mobile && "ml-auto"}`}>
+                {!mobile &&
+                currentUser &&
+                !is_owner &&
+                (following_id ? (
+                    <Button
+                    className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
+                    onClick={() => {}}
+                    >
+                    unfollow
+                    </Button>
+                ) : (
+                    <Button
+                    className={`${btnStyles.Button} ${btnStyles.Black}`}
+                    onClick={() => {}}
+                    >
+                    follow
+                    </Button>
+                ))}
+            </div>
+        </div>
     )
 }
 
