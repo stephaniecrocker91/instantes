68332250bd4c23710338315d53761752d1e89f64
Stephie <stephaniecrocker91@gmail.com>
2022-10-13 08:50:41 +0000

    Display Followers, Following, and posts count in ProfilePage.js

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -46,27 +46,40 @@ function ProfilePage() {
             fetchData();
         }, [id, setProfileData]);
 
-    const mainProfile = (
-        <>
-        <Row noGutters className="px-3 text-center">
-            <Col lg={3} className="text-lg-left">
-            <Image
-            className={styles.ProfileImage}
-            roundedCircle
-            src={profile?.image}
-            />
-            </Col>
-            <Col lg={6}>
-            <h3 className="m-2">Profile username</h3>
-            <p>Profile stats</p>
-            </Col>
-            <Col lg={3} className="text-lg-right">
-            <p>Follow button</p>
-            </Col>
-            <Col className="p-3">Profile content</Col>
-        </Row>
-        </>
-    );
+        const mainProfile = (
+            <>
+                <Row noGutters className="px-3 text-center">
+                    <Col lg={3} className="text-lg-left">
+                    <Image
+                        className={styles.ProfileImage}
+                        roundedCircle
+                        src={profile?.image}
+                    />
+                    </Col>
+                    <Col lg={6}>
+                        <h3 className="m-2">{profile?.owner}</h3>
+                        <Row className="justify-content-center no-gutters">
+                            <Col xs={3} className="my-2">
+                                <div>{profile?.posts_count}</div>
+                                <div>posts</div>
+                            </Col>
+                            <Col xs={3} className="my-2">
+                                <div>{profile?.followers_count}</div>
+                                <div>followers</div>
+                            </Col>
+                            <Col xs={3} className="my-2">
+                                <div>{profile?.following_count}</div>
+                                <div>following</div>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col lg={3} className="text-lg-right">
+                        <p>Follow button</p>
+                    </Col>
+                    <Col className="p-3">Profile content</Col>
+                </Row>
+                </>
+            );
 
     const mainProfilePosts = (
         <>
