bb242aa65f93ac312ea5395bf70a69e3d3ee0aff
Stephie <stephaniecrocker91@gmail.com>
2022-10-13 08:20:45 +0000

    Write useEffect to fetchData from api for Profile

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -11,15 +11,37 @@ import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 
 import PopularProfiles from "./PopularProfiles";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { useCurrentUser } from "../../context/CurrentUserContext";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useParams } from "react-router";
+import {
+    useProfileData,
+    useSetProfileData,
+  } from "../../context/ProfileDataContext";
 
 function ProfilePage() {
     const [hasLoaded, setHasLoaded] = useState(false);
     const currentUser = useCurrentUser();
+    const { id } = useParams();
+    const setProfileData = useSetProfileData();
 
     useEffect(() => {
-        setHasLoaded(true);
-    }, [])
+        const fetchData = async () => {
+            try {
+                const [{ data: pageProfile }] = await Promise.all([
+                axiosReq.get(`/profiles/${id}/`),
+                ]);
+                setProfileData((prevState) => ({
+                ...prevState,
+                pageProfile: { results: [pageProfile] },
+                }));
+                setHasLoaded(true);
+            } catch (err) {
+                console.log(err);
+            }
+            };
+            fetchData();
+        }, [id, setProfileData]);
 
     const mainProfile = (
         <>
