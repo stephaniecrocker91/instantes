24ddfc3ab6f55f9a79c3b09faa867588e985535d
Stephie <stephaniecrocker91@gmail.com>
2022-10-13 08:07:13 +0000

    Create ProfilePage.js and import boilerplate code, and wire up route to App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PostEditForm from './pages/posts/PostEditForm';
 import PostPage from './pages/posts/PostPage';
 import PostsPage from './pages/posts/PostsPage';
 import { useCurrentUser } from "./context/CurrentUserContext";
+import ProfilePage from './pages/profiles/ProfilePage';
 
 
 function App() {
@@ -54,7 +55,7 @@ function App() {
                 <Route exact path="/posts/create" render={() => <PostCreateForm />} />
                 <Route exact path="/posts/:id" render={() => <PostPage />} />
                 <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
-                {/* <Route exact path="/profiles/:id" render={() => <ProfilePage />} /> */}
+                <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
                 <Route render={() => <p>Page not found!</p>} />
             </Switch>
           </Container>
