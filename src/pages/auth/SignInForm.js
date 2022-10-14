c275078b792331dfc34410ea2f8f4a718ec44775
Stephie <stephaniecrocker91@gmail.com>
2022-10-14 09:40:08 +0000

    Autoimport useRedirect hook to SignInForm to redirect users away from the page if logged in

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -15,10 +15,12 @@ import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import { useSetCurrentUser } from "../../context/CurrentUserContext";
+import { useRedirect } from "../../hooks/useRedirect";
 
 function SignInForm() { 
 
     const setCurrentUser = useSetCurrentUser();
+    useRedirect("loggedIn");
 
     const [signInData, setSignInData] = useState({
         username: "",
@@ -41,7 +43,7 @@ function SignInForm() {
         try {
             const { data } = await axios.post("/dj-rest-auth/login/", signInData);
             setCurrentUser(data.user)
-            history.push("/");
+            history.goBack();
         } catch (err) {
             setErrors(err.response?.data);
         }
