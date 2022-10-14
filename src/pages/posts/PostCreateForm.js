f458689e526c46a534e15737c07ae943562be460
Stephie <stephaniecrocker91@gmail.com>
2022-10-14 09:57:55 +0000

    UseRedirect hook to PostCreateForm.js

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -17,9 +17,10 @@ import {Image} from "react-bootstrap"
 
 import { useHistory } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+import { useRedirect } from "../../hooks/useRedirect";
 
 function PostCreateForm() {
-
+    useRedirect("loggedOut");
     const [errors, setErrors] = useState({});
 
     const [postData, setPostData] = useState({
