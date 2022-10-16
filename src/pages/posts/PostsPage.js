54476aab8810bf6d6562fb057fb047ae3602878d
Stephie <stephaniecrocker91@gmail.com>
2022-10-16 17:58:17 +0000

    Import currentUser hook to POstspage , call it in component and add to dependency array

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -16,13 +16,17 @@ import NoResults from "../../assets/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 import PopularProfiles from "../profiles/PopularProfiles";
+import { useCurrentUser } from "../../context/CurrentUserContext"
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
 
+
   const [query, setQuery] = useState("");
+
+  const currentUser = useCurrentUser();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -42,7 +46,7 @@ function PostsPage({ message, filter = "" }) {
     return () => {
       clearTimeout(timer);
     };
-  }, [filter, query, pathname]);
+  }, [filter, query, pathname, currentUser]);
 
   return (
     <Row className="h-100">
