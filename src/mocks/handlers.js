3e50b4803c2805fbef6aa5b77a894d80ffbce814
Stephie <stephaniecrocker91@gmail.com>
2022-10-16 13:29:06 +0000

    Set up handlers.js to get api with json data and return a status 200

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,3 +1,19 @@
 const baseURL = "https://instantes-drf-api.herokuapp.com/";
 
-export const handlers = []
+export const handlers = [
+    rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+        return res(
+            ctx.json({
+                "pk":7,
+                "username":"Jemma",
+                "email":"",
+                "first_name":"",
+                "last_name":"",
+                "profile_id":7,
+                "profile_image":"https://res.cloudinary.com/darmodjdz/image/upload/v1/media/../avatar_profile_dywli5"
+            }));
+    }),
+    rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+        return res(ctx.status(200));
+    }),
+];
